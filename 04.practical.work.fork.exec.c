10039ceffae0ab3dc21ab2fd14470f5f702180c5
KieuViet <themasterofwind1997@gmail.com>
2017-02-20 21:31:10 +0700

    Update 04.practical.work.fork.exec.c

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -1,24 +1,22 @@
-+#include<stdio.h>
-+#include<unistd.h>
-+int main(){
-+	printf("main before fork()\n");
-+	int pid = fork();
-+	if (pid == 0){
-+		printf("I am child after fork(), launching ps -ef\n");
-+		char *args[]= { "/bin/ps", "-ef" , NULL};
-+		execvp("/bin/ps", args);
-+		printf("Finished launching ps -ef\n");
-+		}
-+	else { 
-+		printf("I am parent after fork(), child is %d\n", pid);
-+		int pid = fork();
-+		if (pid == 0){
-+			printf("I am a child after fork(), lauching free -h\n");
-+			char *args[] = {"/bin/free","-h",NULL};
-+			execvp("/usr/bin/free",args);
-+			printf("finish lauching free -h\n");
-+		}
-+	else {
-+		printf("I am parent after fork(), child is %d\n", pid)};
-+return 0;
-+}
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+	printf("main before fork()\n");
+	int pid = fork();
+	if (pid == 0){
+		printf("I am child after fork(), launching ps -ef\n");
+		char *args[]= { "/bin/ps", "-ef" , NULL};
+		execvp("/bin/ps", args);
+		printf("Finish launching ps -ef\n");
+		}
+	else printf("I am parent after fork(), child is %d\n", pid);
+		int pid = fork();
+		if (pid == 0){
+			printf("I am a child after fork(), lauching free -h\n");
+			char *args[] = {"/bin/free","-h",NULL};
+			execvp("/usr/bin/free",args);
+			printf("finish lauching free -h\n");
+		}
+	else printf("I am parent after fork(), child is %d\n", pid);
+return 0;
+}
